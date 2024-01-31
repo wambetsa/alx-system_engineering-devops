233bd6fdd4fa411938a9661abd981805fa714e17
wambetsa emmanuel <wambetsae@gmail.com>
2024-01-31 22:51:58 +0300

    hbt n

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
+
 #Regular expression matching school
 puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#Regular expression repetition token2
+puts ARGV[0].scan(/hbt+n/).join
