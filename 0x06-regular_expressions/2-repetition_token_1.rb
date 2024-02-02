dad4c0e16412479bf7b61ee4663e8830e2552822
wambetsa emmanuel <wambetsae@gmail.com>
2024-02-03 02:20:11 +0300

    match school

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-# Check if an argument is provided
-if ARGV.empty?
-    puts "Usage: #{$PROGRAM_NAME} <string>"
-    exit 1
-  end
-  
-  # Regular expression pattern
-  pattern = /hbt{2,5}n/
-  
-  # Input string
-  input_string = ARGV[0]
-  
-  # Match the input string against the pattern
-  if input_string.match?(pattern)
-    puts "Match found: #{input_string}"
-  else
-    puts "No match found for 'hbt{2,5}n' in the input string."
-  end
+puts ARGV[0].scan(/hb?t?n/).join
