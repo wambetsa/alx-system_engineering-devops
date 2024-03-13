385109b70d49cd23d736891bfd7c83a72334147d
wambetsa <wambetsae@gmail.com>
2024-03-14 02:19:44 +0300

    repetition token

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,16 +1,3 @@
 #!/usr/bin/env ruby
+puts ARGV[0].scan(/School/).join
 
-# Check if the argument matches the regular expression
-def match_school(string)
-    regex = /School/
-    return string.match(regex)
-  end
-  
-  # Accept one argument from the command line
-  input_string = ARGV[0]
-  
-  # Call the match_school method with the input string
-  match_result = match_school(input_string)
-  
-  # Print the matched substring or an empty string
-  puts match_result ? match_result[0] : ""  
