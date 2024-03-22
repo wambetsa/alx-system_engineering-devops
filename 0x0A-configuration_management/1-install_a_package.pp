128bc54cf2643f8ebaba95e85fe837623764f721
wambetsa <wambetsae@gmail.com>
2024-03-22 23:17:15 +0300

    Execute a command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # install flask 2.1.0 using pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install flask':
+  command =>  'pip3 install flask==2.1.0',
+  path    =>  '/usr/bin/',
+  unless  =>  'pip3 list | grep flask',
 }
