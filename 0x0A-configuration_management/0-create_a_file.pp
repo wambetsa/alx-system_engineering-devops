0bf6599998b788295f5470b56935b387602c115a
wambetsa <wambetsae@gmail.com>
2024-03-23 00:00:18 +0300

    changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #create file school
 
 file { '/tmp/school':
-  ensure => 'file',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # install flask 2.1.0 using pip3
+# package
 
-exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
-  path    =>  '/usr/bin/',
-  unless  =>  'pip3 list | grep flask',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
