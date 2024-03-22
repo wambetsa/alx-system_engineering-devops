22212eb8f9b2cedee77010412881abbe18d8777a
wambetsa <wambetsae@gmail.com>
2024-03-22 22:54:34 +0300

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # kills a reunning server program
 
-exec { 'killmenow':
-  command => 'pkill -n killmenow',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => "test $(pgrep killmenow|wc -l) -ge 1",
+exec { 'kill_killmenow':
+  command =>  'pkill -9 killmenow',
+  path    =>  ['/usr/bin/'],
 }
