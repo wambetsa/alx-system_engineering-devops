fb872f029fa30ff0b452810cc63d80866748e3a4
wambetsa <wambetsae@gmail.com>
2024-03-22 16:01:39 +0300

    adding ensure => file to 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 # Puppet code that create file /tmp, of path /tmp/school, permission 0744, owner and group www-data, and content I love Puppet
 
-file {'/tmp/school':
+file { '/tmp/school':
+  ensure  => file,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
