d887d26177c5a2fc88f1315d22baffd539e76c39
wambetsa <wambetsae@gmail.com>
2024-03-22 16:11:12 +0300

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Puppet code that create file /tmp, of path /tmp/school, permission 0744, owner and group www-data, and content I love Puppet
 
 file { '/tmp/school':
-  ensure  => file,
+  ensure  => 'file',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
-  content => "I love Puppet\n",
+  content => 'I love Puppet',
 }
 
