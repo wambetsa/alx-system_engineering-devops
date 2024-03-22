7475b105cb07f10a85205c4c4e591d375fa80caf
wambetsa <wambetsae@gmail.com>
2024-03-22 23:39:42 +0300

    changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #create file school
 
-file { '/tmp/school
+file { '/tmp/school':
   ensure => 'file',
   mode => '0744',
   owner => 'www-data',
   group => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
