d119ea565d41d1ab2ee145739e4531b5b05e7d14
wambetsa <wambetsae@gmail.com>
2024-04-17 13:36:26 +0300

    changed pip3 install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
-# install a specific version of flask (2.1.0) using pip3
+# install a specific version of flask (2.1.0)
 
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
